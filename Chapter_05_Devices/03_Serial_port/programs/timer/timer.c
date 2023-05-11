affc6d1cdcf65f2327be69160439bc914b349725
Ivan Futivić <ivan.futivic@fer.hr>
2023-05-11 07:49:58 +0000

    lab4x

--- Chapter_05_Devices/03_Serial_port/programs/timer/timer.c
+++ Chapter_05_Devices/03_Serial_port/programs/timer/timer.c
@@ -5,83 +5,112 @@
 
 static timespec_t t0;
 
-static void alarm_nt(sigval_t param)
-{
-	int num;
-	timespec_t t;
+static void print_clocks() {
+    timespec_t monotonic_time, real_time;
 
-	num = param.sival_int;
-	clock_gettime(CLOCK_REALTIME, &t);
-	time_sub(&t, &t0);
+	clock_gettime(CLOCK_MONOTONIC, &monotonic_time);
+	time_sub(&monotonic_time, &t0);
 
-	printf("[%d:%d] Alarm %d (every %d seconds)\n",
-		t.tv_sec, t.tv_nsec / 100000000, num, num);
+	clock_gettime(CLOCK_REALTIME, &real_time);
+	time_sub(&real_time, &t0);
+
+    printf("CLOCK_MONOTONIC: %d:%d\n", monotonic_time.tv_sec, monotonic_time.tv_nsec / 100000000);
+    printf("CLOCK_REALTIME:  %d:%d\n", real_time.tv_sec, real_time.tv_nsec / 100000000);
+	printf("-----------------------\n");
 }
 
-int timer()
-{
-	timespec_t t;
-	itimerspec_t t1, t2;
-	timer_t timer1, timer2;
-	sigevent_t evp;
+void alarm_handler1() {
+    print_clocks();
+}
 
-	printf("Example program:[%s:%s]\n%s\n\n", __FILE__, __FUNCTION__,
-		timer_PROG_HELP);
+void alarm_handler2() {
+    timespec_t monotonic_time, real_time;
+
+	clock_gettime(CLOCK_MONOTONIC, &monotonic_time);
+	time_sub(&monotonic_time, &t0);
+
+	clock_gettime(CLOCK_REALTIME, &real_time);
+	time_sub(&real_time, &t0);
+
+    if (monotonic_time.tv_sec > real_time.tv_sec) {
+		printf("+1s\n");
+        real_time.tv_sec += 1;
+    }
+    else if (monotonic_time.tv_sec < real_time.tv_sec) {
+		printf("-1s\n");
+        real_time.tv_sec -= 1;
+    }
+
+    clock_settime(CLOCK_REALTIME, &real_time);
+}
+
+void alarm_handler3() {
+	timespec_t monotonic_time, real_time;
+
+	clock_gettime(CLOCK_MONOTONIC, &monotonic_time);
+	time_sub(&monotonic_time, &t0);
+
+	clock_gettime(CLOCK_REALTIME, &real_time);
+	time_sub(&real_time, &t0);
+
+    if (monotonic_time.tv_sec > real_time.tv_sec) {
+		printf("+10s\n");
+        real_time.tv_sec += 10;
+    }
+    else if (monotonic_time.tv_sec < real_time.tv_sec) {
+		printf("-10s\n");
+        real_time.tv_sec -= 10;
+    }
+
+    clock_settime(CLOCK_REALTIME, &real_time);
+}
+
+int timer() {
+	timespec_t t;
+	itimerspec_t t1, t2, t3;
+	timer_t timer1, timer2, timer3;
+	sigevent_t evp1, evp2, evp3;
 
 	clock_gettime(CLOCK_REALTIME, &t);
 	t0 = t;
-	printf("System time: %d:%d\n", t.tv_sec, t.tv_nsec / 100000000);
 
-	evp.sigev_notify = SIGEV_THREAD;
-	evp.sigev_notify_function = alarm_nt;
-	evp.sigev_notify_attributes = NULL;
+	evp1.sigev_notify = SIGEV_THREAD;
+	evp1.sigev_notify_function = alarm_handler1;
+	evp1.sigev_notify_attributes = NULL;
 
-	/*timer1 */
-	t1.it_interval.tv_sec = 3;
+	t1.it_interval.tv_sec = 1;
 	t1.it_interval.tv_nsec = 0;
-	t1.it_value.tv_sec = 3;
+	t1.it_value.tv_sec = 1;
 	t1.it_value.tv_nsec = 0;
-	evp.sigev_value.sival_int = t1.it_interval.tv_sec;
-	timer_create(CLOCK_REALTIME, &evp, &timer1);
+	evp1.sigev_value.sival_int = t1.it_interval.tv_sec;
+	timer_create(CLOCK_MONOTONIC, &evp1, &timer1);
 	timer_settime(&timer1, 0, &t1, NULL);
 
-	/*timer2 */
-	t2.it_interval.tv_sec = 5;
+	evp2.sigev_notify = SIGEV_THREAD;
+	evp2.sigev_notify_function = alarm_handler2;
+	evp2.sigev_notify_attributes = NULL;
+
+	t2.it_interval.tv_sec = 2;
 	t2.it_interval.tv_nsec = 0;
-	t2.it_value.tv_sec = 5;
+	t2.it_value.tv_sec = 2;
 	t2.it_value.tv_nsec = 0;
-	evp.sigev_value.sival_int = t2.it_interval.tv_sec;
-	timer_create(CLOCK_MONOTONIC, &evp, &timer2);
+	evp2.sigev_value.sival_int = t2.it_interval.tv_sec;
+	timer_create(CLOCK_REALTIME, &evp2, &timer2);
 	timer_settime(&timer2, 0, &t2, NULL);
 
-	t.tv_sec = 11;
-	t.tv_nsec = 0;
+	evp3.sigev_notify = SIGEV_THREAD;
+	evp3.sigev_notify_function = alarm_handler3;
+	evp3.sigev_notify_attributes = NULL;
 
-	while (TIME_IS_SET(&t))
-		if (clock_nanosleep(CLOCK_REALTIME, 0, &t, &t))
-			printf("Interrupted sleep?\n");
+	t3.it_interval.tv_sec = 5;
+	t3.it_interval.tv_nsec = 0;
+	t3.it_value.tv_sec = 5;
+	t3.it_value.tv_nsec = 0;
+	evp3.sigev_value.sival_int = t3.it_interval.tv_sec;
+	timer_create(CLOCK_MONOTONIC, &evp3, &timer3);
+	timer_settime(&timer3, 0, &t3, NULL);
 
-	clock_gettime(CLOCK_REALTIME, &t);
-	printf("System time: %d:%d\n", t.tv_sec, t.tv_nsec / 100000000);
-
-	t.tv_sec = 3;
-	t.tv_nsec = 0;
-	clock_settime(CLOCK_REALTIME, &t);
-	clock_gettime(CLOCK_REALTIME, &t);
-	printf("System time: %d:%d\n", t.tv_sec, t.tv_nsec / 100000000);
-
-	t.tv_sec = 11;
-	t.tv_nsec = 0;
-
-	while (TIME_IS_SET(&t))
-		if (clock_nanosleep(CLOCK_REALTIME, 0, &t, &t))
-			printf("Interrupted sleep?\n");
-
-	clock_gettime(CLOCK_REALTIME, &t);
-	printf("System time: %d:%d\n", t.tv_sec, t.tv_nsec / 100000000);
-
-	timer_delete(&timer1);
-	timer_delete(&timer2);
+	while(1) {continue;}
 
 	return 0;
 }
